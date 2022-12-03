28e9561293df19847a87de6c59f32da3a68836e9
Rajvardhan <singhrajvardhan2003@gmail.com>
2022-12-03 14:02:43 +0530

    added layout and home page

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.set('views', path.join(__dirname, 'views')); // Set the views directory
 // ENDPOINTS
 app.get('/', (req, res) => {
     const params = {}
-    res.status(200).render('index.pug', params);
+    res.status(200).render('home.pug', params);
 })
 
 
